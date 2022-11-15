4fda17d30d3e7e982331622070ffd0d3b2fe8c5a
Beau Pollard <beauppollard@gmail.com>
2022-11-15 08:15:17 -0500

    Making sure versions are consistent for multiple computers

--- scripts/scene_generator.py
+++ scripts/scene_generator.py
@@ -13,6 +13,7 @@ client = RemoteAPIClient()
 sim = client.getObject('sim')
 motors=[]
 sim.closeScene()
+sim.setBoolParam(sim.boolparam_display_enabled,False)
 x_rec=[]
 edge_rec=[]
 sim_results=[]
@@ -34,7 +35,7 @@ while True:
     edge_rec.append(copy.copy(edge_current))
 
 
-with open('nodes.txt', 'w') as convert_file:
+with open('nodes2.txt', 'w') as convert_file:
     for i in x_rec:
         for j in i:
 
@@ -42,6 +43,21 @@ with open('nodes.txt', 'w') as convert_file:
             convert_file.write("\n")
         convert_file.write("\n")
         convert_file.write("\n")
+
+with open('edges2.txt', 'w') as convert_file:
+    for i in edge_rec:
+        for j in i:
+
+            convert_file.write(json.dumps(j))
+            convert_file.write("\n")
+        convert_file.write("\n")
+        convert_file.write("\n")
+
+with open('results2.txt', 'w') as convert_file:
+    for i in sim_results:
+        convert_file.write(json.dumps(i))
+        convert_file.write("\n")
+
 # print('hey')
 # utils.build_planet_wheels(sim,0.25)
 # b0=utils.generate_body(sim,[0.3,0.15,0.2])
